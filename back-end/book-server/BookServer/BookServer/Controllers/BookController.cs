1799c85a002cff6d890d96253e2f3cef1684d7cf
Antonio Velev <toni.velev@abv.bg>
2022-04-28 18:43:46 +0300

    Add controllers

---
+++ back-end/book-server/BookServer/BookServer/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿namespace BookServer.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Services.Book;
+
+    public class BookController : Controller
+    {
+        private readonly IBookService bookService;
+        public BookController(IBookService bookService)
+        {
+            this.bookService = bookService;
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ back-end/book-server/BookServer/BookServer/Controllers/CommentController.cs
@@ -0,0 +1,19 @@
+﻿namespace BookServer.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Services.Comment;
+
+    public class CommentController : Controller
+    {
+        private readonly ICommentService commentService;
+        public CommentController(ICommentService commentService)
+        {
+            this.commentService = commentService;
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
