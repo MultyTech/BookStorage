27cc925b898f1c8ff1a81ef7b182a41569b5a85f
Antonio Velev <toni.velev@abv.bg>
2022-04-28 12:48:30 +0300

    Add home service

---
+++ back-end/book-server/BookServer/BookServer/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+
+        }
+        public IActionResult Home()
+        {
+            //returna all books
+            return this.View();
+        }
+    }
+}
---
+++ back-end/book-server/BookServer/Services/Home/HomeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Home
+{
+    public class HomeService : IHomeService
+    {
+        public IEnumerable<T> GetAllBooks<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
